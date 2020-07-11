bc34689ea035ea1b158b983301219fa566610cac
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-07-11 21:52:04 +0900

    プロフィールページの作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <title>{{ config('app.name', 'Laravelgram') }}</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -17,7 +18,9 @@
 
     @yield('navbar')
 
-    @yield('content')
+    <div class="container">
+      @yield('content')
+    </div>
 
     @yield('footer')
 
