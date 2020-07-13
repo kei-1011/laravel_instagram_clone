ae2b13aabb7fd515e8332a6dea5874d5c48be340
kei-1011 <k_kojima0407@yahoo.co.jp>
2020-07-13 23:37:45 +0900

    コメント機能の実装

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Comment;
+use App\Post;
+use Auth;
+use Validator;
+
+class CommentsController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request) {
+        $comment = new Comment;
+        $comment->comment = $request->comment;
+        $comment->post_id = $request->post_id;
+        $comment->user_id = $request->user_id;
+        $comment->save();
+
+        return redirect('/');
+    }
+
+    public function destroy(Request $request) {
+        $comment = Comment::find($request->comment_id);
+        $comment->delete();
+        return redirect('/');
+    }
+}
